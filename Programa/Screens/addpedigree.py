ea3fc63a691bf56faf32827cfcbb2c5d2a803ea7
Sable <102194057+Thlumyn@users.noreply.github.com>
2026-04-29 16:30:25 -0700

    broken WIP

---
+++ Programa/Screens/addpedigree.py
@@ -0,0 +1,109 @@
+from tkinter import * 
+from tkinter import ttk 
+from tkinter import messagebox
+import banco
+from Screens.screen import Screen
+
+class AddRabbitScreen(Screen):
+        
+    def __init__(self):
+        columns_new_rabbits = {
+            "name": "Name",
+            "earno": "Ear #",
+            "breed": "Breed",
+            "variety": "Variety",
+            "weight": "Weight",
+            "reg": "Reg #",
+            "gc": "GC #",
+            "legs": "Legs",
+            "dob": "DOB"
+        }
+        entrylist = list(columns_new_rabbits.keys())
+        entry = {}
+
+
+    def update_rabbit(self, app1, id = 0):
+        rabbitdata = {}
+        for entrycolumn in self.entrylist:
+            rabbitdata[entrycolumn] = self.entry[entrycolumn].get()
+        if id > 0 and int(id) == id:
+            newlist = ''
+            for eachcol in self.entrylist:
+                if newlist != '':
+                    newlist = newlist + ', '
+                newlist = newlist + eachcol + " = '" + rabbitdata[eachcol] + "'"
+            sqlstring = 'UPDATE rabbits SET ' + newlist + ' WHERE r_id = ' + str(id)
+        else:
+            selectlist = ''
+            newlist = ''
+            for eachcol in self.entrylist:
+                if selectlist != '':
+                    selectlist = selectlist + ', '
+                selectlist = selectlist + eachcol
+                if newlist != '':
+                    newlist = newlist + "', '"
+                newlist = newlist + rabbitdata[eachcol]
+            sqlstring = 'INSERT into rabbits (' + selectlist + ", status) values ('" + newlist + "', '1')"
+        #print (sqlstring)
+        try:
+            banco.dml(sqlstring)
+            app1.destroy()
+        except:
+            messagebox.showinfo(title = "ERROR", message = "Error saving data")
+
+
+    def add_pedigree(self, id=0):
+        update = False
+        if id != 0 and int(id) == id:
+            update = True
+            selectlist = ''
+            for eachcol in list(self.columns_new_rabbits.keys()):
+                if selectlist != '':
+                    selectlist = selectlist + ', '
+                selectlist = selectlist + eachcol
+            vquery = "SELECT " + selectlist + " FROM rabbits WHERE r_id = '" + str(id) + "'"
+            rows = banco.dql(vquery)
+            rows = rows[0]
+            rabbitdata = {}
+            for eachcol in list(self.columns_new_rabbits.keys()):
+                rabbitdata[eachcol] = rows[list(self.columns_new_rabbits.keys()).index(eachcol)]
+            
+
+            #for i in rows:
+            #    tv.insert("", "end", values = i)
+
+
+
+        app1 = Toplevel()
+        app1.title("Modify Rabbit")
+        app1.geometry("240x380")
+        quadUpdate = LabelFrame(app1, text = "Modify Rabbit")
+        quadUpdate.pack(fill = "both", expand = "yes", padx = 10, pady = 5)
+
+        placementx = 5
+        placementy = 5
+        for entrycolumn in self.entrylist:
+            self.entry[entrycolumn] = Entry(quadUpdate)
+            self.entry[entrycolumn].place(x = placementx + 50,y = placementy)
+
+            #Add prefilled values if updating rabbits
+            if update:
+                if rabbitdata[entrycolumn] == None:
+                    rabbitdata[entrycolumn] = ''
+                self.entry[entrycolumn].insert(END,rabbitdata[entrycolumn]) 
+
+            lbentry = Label(quadUpdate, text = self.columns_new_rabbits[entrycolumn])
+            lbentry.pack(side = "left")
+            lbentry.place(x = placementx, y = placementy)
+            placementy += 35
+
+        if update:
+            btn_text = "Update"
+        else:
+            btn_text = "Add New"
+        btn_Save = Button(quadUpdate, text = btn_text, command = lambda: self.update_rabbit(app1, id))
+        btn_Save.place(x = placementx, y = placementy)
+        btn_Exit = Button(quadUpdate, text = "Cancel", command = app1.destroy)
+        btn_Exit.place(x = placementx + 60, y = placementy)
+
+        app1.mainloop()
---
+++ Programa/Screens/mainscreen.py
@@ -0,0 +1,151 @@
+from tkinter import * 
+from tkinter import ttk 
+from tkinter import messagebox
+import banco
+from Screens.screen import Screen
+from Screens.addpedigree import AddRabbitScreen
+
+class MainScreen(Screen):
+
+    def __init__(self):
+        self.datatype = "rabbits" #used to switch between breeder and rabbit views
+        self.viewstatus = 1 #used to toggle between viewing different statuses
+        #used to store grid values for rabbits vs breeders
+        self.columns_breeders = {
+            "b_id": "ID",
+            "name": "Name",
+            "rabbitry": "Rabbitry",
+            "address": "Address"
+        }
+        self.columns_rabbits = {
+            "r_id": "ID",
+            "name": "Name",
+            "earno": "Ear #",
+            "variety": "Variety",
+            "dob": "DOB"
+        }
+
+    #handle switching between rabbit view and breeder view on grid
+    def switchtobreeders(self):
+        self.datatype = "breeders"
+        self.refresh_headers()
+        self.populate()
+        self.quadroGrid.configure(text=self.datatype.capitalize())
+    def switchtorabbits(self):
+        self.datatype = "rabbits"
+        self.refresh_headers()
+        self.populate()
+        self.quadroGrid.configure(text=self.datatype.capitalize())
+
+    #populat grid
+    def populate(self):
+        selectlist = ''
+        self.tv.delete(*self.tv.get_children())
+        if self.datatype == "rabbits":
+            for eachcol in list(self.columns_rabbits.keys()):
+                if selectlist != '':
+                    selectlist = selectlist + ', '
+                selectlist = selectlist + eachcol
+            vquery = "SELECT " + selectlist + " FROM rabbits WHERE status = 1 order by r_id ASC"
+        else: 
+            for eachcol in list(self.columns_breeders.keys()):
+                if selectlist != '':
+                    selectlist = selectlist + ', '
+                selectlist = selectlist + eachcol
+            vquery = "SELECT " + selectlist + " FROM breeders WHERE status = 1 order by b_id ASC"
+        rows = banco.dql(vquery)
+        for i in rows:
+            self.tv.insert("", "end", values = i)
+
+    def search(self):
+        self.tv.delete(*self.tv.get_children())
+        vquery = "SELECT * FROM rabbits WHERE name LIKE '%" + self.vnamesearch.get() + "%'"
+        lines = banco.dql(vquery)
+        for i in lines:
+            self.tv.insert("", "end", values = i)
+        self.vnamesearch.delete(0, END)
+
+
+    def get_headers(self):
+        #Set up columns
+        if self.datatype == "rabbits":
+            headers = self.columns_rabbits
+        else:
+            headers = self.columns_breeders
+        return headers
+
+    def refresh_headers(self):
+        self.tv.delete(*self.tv.get_children())
+        headers = self.get_headers()
+        columns = list(headers.keys())
+
+        self.tv["columns"] = columns
+        
+        for eachcol in columns:
+            self.tv.column(eachcol, minwidth = 0, width = 100)
+            self.tv.heading(eachcol, text = headers[eachcol])
+
+    def get_selected_item(self):
+        curItem = self.tv.focus()
+        if len(curItem) > 0:
+            return self.tv.item(curItem)["values"][0]
+        else:
+            return 0
+
+    def open_main_screen(self):
+
+        addpedigree = AddRabbitScreen()
+        #Set up heading
+        app = Tk()
+        app.title("Open Rabbit Pedigree System")
+        app.geometry("600x500")
+
+        #Set up menu bar
+        app.option_add('*tearOff', FALSE)
+        #toplevel = Toplevel(app)
+        m = Menu(app)
+        m_options = Menu(m)
+        m_data = Menu(m)
+        m.add_cascade(menu=m_options, label = "Settings")
+        m_options.add_command(label="test1")
+        m_options.add_command(label="test2")
+        m.add_cascade(menu=m_data, label = "Data")
+        m_data.add_command(label="View Rabbits", command=self.switchtorabbits)
+        m_data.add_command(label="View Breeders", command=self.switchtobreeders)
+        app['menu'] = m
+
+        #Set up Grid
+        quadroGrid = LabelFrame(app, text = self.datatype.capitalize())
+        quadroGrid.pack(fill = "both", expand = "yes", padx = 10, pady = 10)
+
+        headers = self.get_headers()
+        columns = list(headers.keys())
+        tv = ttk.Treeview(quadroGrid, columns = columns, show = 'headings')
+
+        self.refresh_headers()
+
+        tv.pack()
+        self.populate()
+
+        #Set up Insert Form
+        quadInsert = LabelFrame(app, text = "Insert New Content")
+        quadInsert.pack(fill = "both", expand = "yes", padx = 10, pady = 5)
+
+        btn_addnew = Button(quadInsert, text = "Add Rabbit", command = addpedigree.add_pedigree)
+        btn_addnew.place(x = 50, y = 0)
+        btn_test = Button(quadInsert, text = "Modify Rabbit", command = lambda: addpedigree.add_pedigree(self.get_selected_item()))
+        btn_test.place(x = 150, y = 0)
+
+        #Set up Search Form
+        quadSearch = LabelFrame(app, text = "Search")
+        quadSearch.pack(fill = "both", expand = "yes", padx= 10, pady = 10)
+        lbid =Label(quadSearch, text = "Name")
+        lbid.pack(side = "left")
+        vnamesearch = Entry(quadSearch)
+        vnamesearch.pack(side = "left", padx = 10)
+        btn_search = Button(quadSearch, text = "Search", command = self.search)
+        btn_search.pack(side = "left", padx = 10)
+        btn_all = Button(quadSearch, text = "Show All", command = self.populate)
+        btn_all.pack(side = "left", padx = 10)
+
+        app.mainloop()
