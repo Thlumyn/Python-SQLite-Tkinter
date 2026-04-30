a39573970fa03186b546f079eb0ad97d7d222958
Sable <102194057+Thlumyn@users.noreply.github.com>
2026-04-30 09:33:21 -0700

    add 3 gen to link pedigree page

--- Programa/Screens/linkpedigree.py
+++ Programa/Screens/linkpedigree.py
@@ -49,7 +49,7 @@ class LinkPedigreeScreen(Screen):
 
         self.app1 = Toplevel()
         self.app1.title("Pedigree Linkage")
-        self.app1.geometry("240x380")
+        self.app1.geometry("380x400")
         quadUpdate = LabelFrame(self.app1, text = "Link Pedigree")
         quadUpdate.pack(fill = "both", expand = "yes", padx = 10, pady = 5)
 
@@ -73,9 +73,39 @@ class LinkPedigreeScreen(Screen):
         dddam = self.subRabbit(dsire.dam)
 
         rabbitlb =Label(quadUpdate, text = rabbit.name)
-        rabbitlb.place(x = 5, y = 190)
+        rabbitlb.place(x = 5, y = 180)
 
         sirelb =Label(quadUpdate, text = sire.name)
-        sirelb.place(x = 50, y = 100)
+        sirelb.place(x = 50, y = 85)
         damlb =Label(quadUpdate, text = dam.name)
-        damlb.place(x = 50, y = 280)
+        damlb.place(x = 50, y = 260)
+
+        ssirelb =Label(quadUpdate, text = ssire.name)
+        ssirelb.place(x = 120, y = 45)
+        sdamlb =Label(quadUpdate, text = sdam.name)
+        sdamlb.place(x = 120, y = 125)
+
+        dsirelb =Label(quadUpdate, text = dsire.name)
+        dsirelb.place(x = 120, y = 215)
+        ddamlb =Label(quadUpdate, text = ddam.name)
+        ddamlb.place(x = 120, y = 305)
+
+        sssirelb =Label(quadUpdate, text = sssire.name)
+        sssirelb.place(x = 210, y = 15)
+        ssdamlb =Label(quadUpdate, text = ssdam.name)
+        ssdamlb.place(x = 210, y = 60)
+
+        sdsirelb =Label(quadUpdate, text = sdsire.name)
+        sdsirelb.place(x = 210, y = 105)
+        sddamlb =Label(quadUpdate, text = sddam.name)
+        sddamlb.place(x = 210, y = 150)
+
+        dssirelb =Label(quadUpdate, text = dssire.name)
+        dssirelb.place(x = 210, y = 195)
+        dsdamlb =Label(quadUpdate, text = dsdam.name)
+        dsdamlb.place(x = 210, y = 240)
+
+        ddsirelb =Label(quadUpdate, text = ddsire.name)
+        ddsirelb.place(x = 210, y = 285)
+        dddamlb =Label(quadUpdate, text = dddam.name)
+        dddamlb.place(x = 210, y = 330)
